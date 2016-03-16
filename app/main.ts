5e981e16534b39299eb9f771921f4458d4fc7152
ratscrew <ratscrew2002@yahoo.com>
2016-03-16 23:50:22 +0000

    try

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,13 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './components/myApp/app.component'
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {provider} from  'ng2-redux';
+import {rootReducer} from './reducers';
 
-bootstrap(AppComponent);
+//const provider = require('ng2-redux')
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+bootstrap(AppComponent,[provider(store)]);
