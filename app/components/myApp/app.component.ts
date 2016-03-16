ce6618b58e1c4f644741d2876c34e55a2b49b6f6
ratscrew <ratscrew2002@yahoo.com>
2016-03-16 20:01:27 +0000

    first change

--- app/components/myApp/app.component.ts
+++ app/components/myApp/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>My First Angular 2 App testing</h1>`
+    template: `<h1>My First Angular 2 App testing Redux out :)</h1>`
 })
 export class AppComponent { }
